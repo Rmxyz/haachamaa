ead46aeb672e0d206e1bfc96d3f32f784640bf00
Rmxyz <78423844+Rmxyz@users.noreply.github.com>
2021-11-11 12:42:59 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -464,7 +464,7 @@ global.dfail = (type, m, conn, usedPrefix) => {
     private: 'Perintah ini hanya dapat digunakan di Chat Pribadi!',
     admin: 'Perintah ini hanya untuk *Admin* grup!',
     botAdmin: 'Jadikan bot sebagai *Admin* untuk menggunakan perintah ini!',
-    unreg: `Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama|umur*\n\nContoh: *#daftar Haachamaa|19*`
+    unreg: `Silahkan daftar untuk menggunakan fitur ini dengan cara mengetik:\n\n*#daftar nama|umur*\n\nContoh: *#daftar Manusia|19*`
   }[type]
   if (msg) return m.reply(msg)
 }
