bf198183d55ee152927f87ddb74b1042b9a78205
Rmxyz <78423844+Rmxyz@users.noreply.github.com>
2021-11-11 12:43:39 +0700

    Update haachamaa.js

--- haachamaa.js
+++ haachamaa.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Haachamaaa\nUwuwuwuululu', {
+CFonts.say('Bootxyz\nSimple bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['cyan', 'magenta']
